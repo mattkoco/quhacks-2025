eaec8441ae04b9ad5d704ffa40dbc63a5358207b
DimitriKok <mimikokkinakos@gmail.com>
2024-12-14 10:51:21 -0500

    webstuff

--- my-appcd/src/routes/+page.svelte
+++ my-appcd/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div id=background>
+    <div id=text>
+        Cert Helper
+    </div>
+</div>
+
+<style>
+    #background {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: azure;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #text {
+        font-size: 80px;
+    }
+</style>
