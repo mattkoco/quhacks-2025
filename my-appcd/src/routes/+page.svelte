86b28c193d06ff57ddd57dd8fa96ebb7b2a8925c
DimitriKok <mimikokkinakos@gmail.com>
2024-12-14 12:01:58 -0500

    good chunk of home page is done

--- my-appcd/src/routes/+page.svelte
+++ my-appcd/src/routes/+page.svelte
@@ -4,27 +4,61 @@
 
 <Nav></Nav>
 
-<div id=background>
-    <div id=text>
-        Cert Helper
+<div id=backgroundTop>
+    <div id=title>
+        Cert-<br>Breeze
+    </div>
+</div>
+<div id="backgroundMid">
+    <div id="desc">
+        Cert-Breeze is an application designed to make the certification studying<br> process a breeze, by providing endless practice questions for the certification<br> of your choice, in an easy-to-use, multiple choice format.
     </div>
 </div>
 
 <style>
-    #background {
+
+    @import url(https://fonts.googleapis.com/earlyaccess/nicomoji.css);
+    @import url(https://fonts.googleapis.com/css?family=Pavanam);
+
+    #backgroundTop {
         position: fixed;
-        top: 0;
+        top: 20%;
+        bottom:40%;
+        left: 0;
+        right: 0;
+        background-color: #3A6D8C;
+        display: flex;
+        align-items: center;
+        z-index: 1;
+    }
+  
+
+    #backgroundMid {
+        position: fixed;
+        top: 60%;
         bottom: 0;
         left: 0;
         right: 0;
-        background-color: #2c556f;
+        background-color: #6A9AB0;
         display: flex;
         justify-content: center;
         align-items: center;
+        z-index: 2;
     }
 
-    #text {
-        font-size: 80px;
-        color: aliceblue;
+    #title {
+        font-size: 110px;
+        color: #EAD8B1;
+        left: 0;
+        padding-left: 0.5em;
+        font-family: 'Nico Moji';
+    }
+
+    #desc {
+        font-size: 40px;
+        color: #EAD8B1;
+        text-align: center;
+        font-family: 'Pavanam';
+        font-weight: 100;
     }
 </style>
--- my-appcd/src/routes/nav.svelte
+++ my-appcd/src/routes/nav.svelte
@@ -1,47 +1,20 @@
-<body>
-    <nav id=nav>
-        <div id=navText>
-            <a href="use later" class="navText">add smth</a>
-            <a href="use later" class="navText">add smth</a>
-            <a href="use later" class="navText">add smth</a>
-        </div>
-    </nav>
-</body>
+
+<div id=nav>
+
+</div>
+
 
 <style>
-    * {
-        box-sizing: border-box;
-        z-index: 50;
-    }
-
-    body {
-        margin: 0;
-        padding: 0;
-    }
-
     #nav {
-        position: sticky;
-        padding: 0 2.8em;
-        padding-bottom: 1em;
-        padding-top: 1em;
-        background-color: black;
-        font-size: 1.5em;
-        border-bottom: solid 0.13em white;
+        position: fixed;
+        top: 0;
+        bottom: 80%;
+        left: 0;
+        right: 0;
+        background-color: #001F3F;
         display: flex;
+        justify-content: center;
         align-items: center;
+        z-index: 6;
     }
-
-    #navText {
-        display: flex;
-        gap: 1.5em;
-        font-weight: 600;
-        flex-grow: 1;
-    }
-
-    #navText > *{
-        color: white;
-        text-decoration: none;
-    }
-
-
 </style>
